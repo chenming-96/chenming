868e87ca9acb786c0df5d541de3f6d95b69c7fe1
fanchaohua <fanchaohua@camall.com>
2019-10-30 16:07:43 +0800

    权限模块更改

--- MallAdmin-web/src/components/jurisdiction/sys.js
+++ MallAdmin-web/src/components/jurisdiction/sys.js
@@ -1,18 +1,73 @@
 import axios from "axios";
-// import * as api from "./api";
+// import * as api from "../api";
 import defaultValue from "./default";
 
+export function login (params) {
+  return new Promise((resolve, reject) => {
+    axios.post("/login", { params }).then(response => {
+      resolve(response.data);
+    }, err => {
+      resolve(defaultValue.login);
+    })
+      .catch((error) => {
+        resolve(defaultValue.login)
+      })
+  })
+}
+export function msgList (params) {
+  return new Promise((resolve, reject) => {
+    axios.post('/messageList', { params }).then(response => {
+      resolve(response.data);
+    }, err => {
+     
+      resolve(defaultValue.msgList);
+    })
+      .catch((error) => {
+        resolve(defaultValue.msgList)
+      })
+  })
+}
 
 
-export function menuList (params) {
+
+
+export function resourceList (params) {
+  return new Promise((resolve, reject) => {
+    axios.get("/sys/resource/list", { params }).then(response => {
+      resolve(response.data);
+    }, err => {
+      resolve(defaultValue.resource);
+    })
+      .catch((error) => {
+        resolve(defaultValue.resource)
+      })
+  })
+}
+export function roleList (params) {
   return new Promise((resolve, reject) => {
     axios.get("./Vue-Admin/sys/menu/list", { params }).then(response => {
       resolve(response.data);
     }, err => {
-      resolve(defaultValue.menuList);
+      console.log(defaultValue.roleList)
+      resolve(defaultValue.roleList);
     })
       .catch((error) => {
-        resolve(defaultValue.menuList)
+        console.log(defaultValue.roleList)
+        resolve(defaultValue.roleList)
+      })
+  })
+}
+
+export function userList (params) {
+  const userList = {total:defaultValue.userList.total,records:defaultValue.userList.records.reverse()}
+  return new Promise((resolve, reject) => {
+    axios.get("./Vue-Admin/sys/user/page", { params }).then(response => {
+      resolve(response.data);
+    }, err => {
+      resolve(userList);
+    })
+      .catch((error) => {
+        resolve(userList)
       })
   })
 }
